9a6bd896224253e436a562f02b3728ca14f5ba57
SURU EMMANUEL <suruaino@gmail.com>
2024-12-20 20:50:23 +0100

    made few changes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(express.json)
 app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
